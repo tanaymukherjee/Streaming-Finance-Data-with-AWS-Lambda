d35458d281cfe426ec1733d4b32e751700be7672
Tanay Mukherjee <its_tanay_here@yahoo.com>
2020-05-24 20:28:19 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,5 +1,5 @@
 WITH CTE AS (
-  SELECT *, ROW_NUMBER() over (PARTITION BY High, Hour ORDER BY Name, Hour) AS rn FROM (
+  SELECT *, ROW_NUMBER() over (PARTITION BY High, Hour ORDER BY Name, Hour, Timestamp) AS rn FROM (
     SELECT upper(db1.Name) AS Name, round(db1.High,2) AS High, db1.Hour as Hour, ts AS Timestamp FROM (
       SELECT name AS Name, max(high) AS High, substring(ts,12,2) AS Hour FROM  "04" 
       GROUP BY 1, 3
@@ -12,4 +12,4 @@ WITH CTE AS (
     when rn=1 then (SELECT count(*) FROM CTE t1 WHERE t1.High = t2.High AND t1.Hour = t2.Hour)
     else 0
     end AS Recurrence
-    FROM CTE t2 ORDER BY Name, Hour
+    FROM CTE t2 ORDER BY Name, Hour, Timestamp
